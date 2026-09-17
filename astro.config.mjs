26f679034ce0a53cde7f915e6847a488fb139e29
mkhafidm <mkhafid0502@gmail.com>
2026-09-17 12:25:42 +0700

    chore: set production site URL to mkhafidm.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import compress from 'astro-compress';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  site: 'https://garrettheath4.com/',
+  site: 'https://mkhafidm.netlify.app/',
   output: 'static',
   trailingSlash: 'always',
 
